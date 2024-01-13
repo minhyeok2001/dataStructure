12bcac031949bbd7436f9258f3c3494a5bb446c3
노민혁 <mhroh01@naver.com>
2024-01-13 12:37:39 +0900

    1/13 commit

--- linkedList.c
+++ linkedList.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 /* implementation
-Linked lists, chains, circular list
+Linked lists
 Fundamental function ; insert, delete and advanced ver with avail pointer.
-doubly linked list
+doubly linked list with circularity
 */
 typedef struct node* listPointer;
 typedef struct node {
@@ -13,9 +13,17 @@ typedef struct node {
     listPointer link;
 }node;
 
+typedef struct dlcl* dlclPointer;
+typedef struct dlcl {
+    char* data;
+    dlclPointer rLink;
+    dlclPointer lLink;
+}dlcl;
+
 listPointer start;
 listPointer end; // similar to filepointer or stack pointer etc... showing current pointer.
 listPointer ptr;
+listPointer avail; 
 
 int count=0;
 
@@ -37,7 +45,7 @@ void insert(char* msg, listPointer prev, int value) {
     if (!value) end = temp;
 }
 
-void delete(listPointer prev) {
+void delete(listPointer prev) { 
     if (!prev || !prev->link) {
         return;
     }
@@ -47,19 +55,59 @@ void delete(listPointer prev) {
     free(temp);       // Free the node itself
 }
 
+void availInsert(char* msg, listPointer prev) {
+    // insert node with availPointer
 
-void findWord(char* msg, int judge) {   // 'judge' is used to consider count or not 
+    if(avail) {
+        listPointer temp = avail;
+        avail = avail->link;
+        temp->link = prev->link;
+        prev->link = temp;
+    }
+
+    else {
+        // same as the original insert. memory allocation....
+    }
+}
+
+void availDelete(listPointer prev) {
+    listPointer temp = prev->link;
+    prev->link = temp->link;
+    temp->link = avail;
+    avail = temp;
+}
+
+void dlclInsert(char* msg, dlclPointer prev) {
+    dlclPointer temp = (dlclPointer)malloc(sizeof(dlcl));
+    temp->data = strdup(msg);
+    
+    prev->rLink = temp;
+    temp->lLink = prev;
+    prev->rLink->lLink = temp;
+    temp->rLink = prev->rLink;  // caution !! not prev->rLink->rLink !!
+}
+
+void dlclDelete(dlclPointer temp) { // unlike original linked list deletion, we can use dlclPointer which should be deleted.
+    temp->lLink->rLink = temp->rLink;
+    temp->rLink->lLink = temp->lLink;
+    free(temp);
+}
+
+void findWord(char* msg, int judge) {   // 'judge' is used to consider whether it is count or not 
     count = 0;
-    for (listPointer temp = start; temp; temp = temp->link) {
-        count++;
-        if (strcmp(msg, temp->data) == 0) {
+    for (listPointer temp = start; temp; temp = temp->link) {        
+        if(judge == count) {
             ptr = temp;
             return;
         }
-        if (!(temp->link)) return;
-        
-        if(judge) 
-            if(judge == count) return;
+        else {
+            count++;
+            if (strcmp(msg, temp->data) == 0) {
+                ptr = temp;
+                return;
+            }   
+            if (!(temp->link)) return;
+        }       
     }
 }
 
@@ -94,7 +142,7 @@ int main() {
             }
 
             else {
-                findWord(positionWord,0);
+                findWord(positionWord,-1);
                 if (ptr) {
                     insert(inputWord, ptr, 1);
                     ptr = NULL;
@@ -110,12 +158,20 @@ int main() {
             printf("\n");
             
             ptr = NULL;
-            findWord(inputWord,0);
+            findWord(inputWord,-1);
 
             if (ptr) {
                 int a = count-1;
-                findWord(inputWord,a);
-                delete(ptr);
+                if(a) {
+                    findWord(inputWord,a);
+                    delete(ptr);
+                }
+                else {
+                    listPointer temp = ptr;
+                    start = temp->link;
+                    free(temp->data);
+                    free(temp);
+                }
                 ptr = NULL;
             }
                 
@@ -126,7 +182,7 @@ int main() {
         else if (input == 'f') {
             printf("word : ");
             scanf("%s", positionWord);
-            findWord(positionWord,0);
+            findWord(positionWord,-1);
             printf("\n");
         }
 
