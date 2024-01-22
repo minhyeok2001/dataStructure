e6e22006e0560ab8d48c8b80bb6f615e053a295b
노민혁 <mhroh01@naver.com>
2024-01-22 12:51:25 +0900

    before lunch 1/22

--- graph.c
+++ graph.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define WEIGHTNUM 11
+#define NODENUM 9
+#define TRUE 1
+#define FALSE 0
+
 /*
 Graph traversal ; depth first search, breath first search with adjacency list
 
@@ -19,6 +24,15 @@ listPointer graphPointer[9];
 listPointer curr;
 
 int dupArr[9] = { 0, };  // duplication checking arr.
+
+int queue[9] = { 0, };
+int front = 0;   // front, rear for queue push & pop operation
+int rear = 0;
+
+int** arr = NULL;
+int count=0;
+
+unionFindArr[9];
 
 void add(listPointer prev, int num) {
     listPointer temp = (listPointer)malloc(sizeof(list));
@@ -91,6 +105,115 @@ void dfs(int startingNum) {    //depth first search.
     }
 }
 
+void push(int num) {    // both push and pop are implemented without exception handler
+    queue[rear++] = num;
+}
+
+int pop() {
+    return queue[front++];
+}
+
+void bfs(int startingNum) { //breadth first search
+    //using queue
+    listPointer temp = NULL;
+    int index = startingNum;
+    for (int i = 0;i<9;i++) dupArr[i] = 0; // check duplication
+    printf("%d ", startingNum);
+    dupArr[startingNum] = 1;
+
+    while (1) {
+        if (front == rear && front != 0) break;
+        for (temp = graphPointer[index]->link; temp; temp = temp->link) {
+            if (!dupArr[temp->data]) {
+                push(temp->data);
+                printf("%d ", temp->data);
+                dupArr[temp->data] = 1;
+            }
+            if (!temp->link) break;
+        }
+        index = pop();
+    }
+}
+
+void addArray(int start, int end, int weight) {
+    arr[count][0]=start;
+    arr[count][1]=end;
+    arr[count++][2]=weight;
+}
+
+void setArray () {
+    arr =(int**)malloc(sizeof(int*)*WEIGHTNUM);
+    for(int i=0;i<WEIGHTNUM;i++) {
+        *(arr+i) = (int*)malloc(sizeof(int)*3);
+    }
+    addArray(0,1,3);
+    addArray(0,3,2);
+    addArray(0,8,5);
+    addArray(1,7,4);
+    addArray(2,3,6);
+    addArray(2,7,7);
+    addArray(2,5,1);
+    addArray(3,4,11);
+    addArray(4,8,10);
+    addArray(4,5,8);
+    addArray(5,6,9);
+}
+
+void bubble() {
+	int temp=0;
+	for (int i = 0; i < WEIGHTNUM; i++) {
+		for (int j = 0; j < WEIGHTNUM - i - 1; j++) {
+			if (arr[j][2] > arr[j + 1][2]) {
+				for (int k = 0; k < 3; k++) {
+					temp = arr[j][k];
+					arr[j][k] = arr[j + 1][k];
+					arr[j + 1][k] = temp;
+				}
+			}
+		}
+	}
+}
+
+int unionSets(int a, int b){
+    if(find(a) == find(b)) return TRUE;
+    else return FALSE;
+}
+
+int find(int var) {
+    if(unionFindArr[var]==var) return var;
+    else return find(unionFindArr[var]);
+}
+
+// 주의 !! 작은걸로 하는게 아니라 먼저 들어온 순서대로 해야함 !!
+void makeUnionSets(int a, int b) {
+    if(find(a) > find(b)) unionFindArr[a]=b;
+    else unionFindArr[b]=a;
+    // 숫자는 클 수 있어도, 해당 숫자의 대표가 자기보다 작으면 큰 숫자를 따라가야함.
+}
+
+void kruskal() { // find min weight which does not make circle and connect
+    setArray(); // start, end, weight in double dimension arr
+    // use bubble sort to make arr ascending order.
+    bubble();
+    // union-find ; just like tree...
+    // we put two number in union function, which makes them 'union'
+    int weight=0;
+    
+    for(int i=0;i<NODENUM;i++) unionFindArr[i]=i;
+
+    printf("kruskal : [ ");
+
+    for(int i=0;i<WEIGHTNUM;i++) {
+        if(!unionSets(arr[i][0],arr[i][1])) {
+            printf("(%d,%d) ",arr[i][0],arr[i][1]);
+            makeUnionSets(arr[i][0],arr[i][1]);
+            weight+=arr[i][2];
+        }
+    }
+
+    printf("] weight : %d",weight);
+
+}
 int main() {
 
     setAdjacencyList();
@@ -98,6 +221,13 @@ int main() {
     printf("dfs : ( ");
     dfs(0);
     printf(")\n");
+
+    printf("bfs : ( ");
+    bfs(0);
+    printf(")\n");
+
+    kruskal();
+
 
     return 0;
 }
